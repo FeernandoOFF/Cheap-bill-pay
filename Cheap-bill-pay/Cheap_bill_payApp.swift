60f2d0ee94dd06baaed4daf93fbf209a05da6f50
Fernando <ferobregon03@gmail.com>
2023-04-06 17:16:43 +0100

    Initial Commit

---
+++ Cheap-bill-pay/Cheap_bill_payApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cheap_bill_payApp.swift
+//  Cheap-bill-pay
+//
+//  Created by Fernando Obregon on 06/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Cheap_bill_payApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
