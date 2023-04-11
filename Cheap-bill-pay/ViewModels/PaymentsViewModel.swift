036c7abdfbd3494c9c0394ed16f8372d44fccd4b
Fernando <ferobregon03@gmail.com>
2023-04-11 10:11:32 +0100

    Added README

--- Cheap-bill-pay/ViewModels/PaymentsViewModel.swift
+++ Cheap-bill-pay/ViewModels/PaymentsViewModel.swift
@@ -12,10 +12,14 @@ final class PaymentsViewModel: ObservableObject {
     
     
     @Published var payments: [Payment] = [
-        Payment(id: "1", title: "20 Mar 2023", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
-        Payment(id: "2", title: "Test", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
-        Payment(id: "3", title: "Test 2", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
-        Payment(id: "4", title: "Payment", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
+        Payment(id: "1", title: "Payment test 1", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
+        Payment(id: "2", title: "Bill test 1", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
+        Payment(id: "3", title: "Bill test 2", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
+        Payment(id: "4", title: "Payment test 2", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
+        Payment(id: "5", title: "Payment test 5", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
+        Payment(id: "6", title: "Payment test 6", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type: "Payment"),
+        Payment(id: "7", title: "Bill test 3", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
+        Payment(id: "8", title: "Bill test 4", description: "One off payment", icon:"doc.plaintext.fill" , rightLabel: "€130.88",type:"Bill"),
     ]
 
     
---
+++ README.md
@@ -0,0 +1,22 @@
+# Cheap Bill Pay
+A 'Bill Pay' Clone made with swiftUI, based on the designs and added a little bit of my creativity
+
+## Features
+
+- Charts
+- PDF Viewer
+- Small Animations
+- DarkMode
+- API Calls & Loading state
+
+## Code Structure
+
+- MVVM Design Pattern
+- ViewModifiers for shared modifiers  (Mainly NavigationView Buttons)
+
+## Known Issues
+
+- TabView not working property in iPad
+- Some data is Hard-coded - although thanks for the MVVM patter is easy to just make an API call in the NetworkManager
+- Chart animations get slow when you add more
+
